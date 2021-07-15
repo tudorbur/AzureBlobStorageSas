c77ff6a0e04f4c8066510a2fa7b31643aefb0408
Tudor <tudorb20@gmail.com>
2021-07-15 15:10:25 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Blobs;
+using AzureBlobStorageSas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorageSas.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private IConfiguration _iconfiguration;
+
+    private BlobContainerClient _container;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
